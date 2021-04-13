51490de0a21498a9fc265c10ba6ca7e881893a8e
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-04-13 12:02:36 +0300

    tried to implement routing but now it doesn't work

--- teamlearn/src/App.vue
+++ teamlearn/src/App.vue
@@ -5,12 +5,38 @@
 
 <script>
 
+const NotFound = { template: '<h1>Page not found</h1>'}
+const Home = { template: <MainPage />}
+const About = { template: <About />}
+const Contact = { template: <Contact />}
+const Coding = { template: <Coding />}
+
+
+const routes = {
+  '/': Home,
+  '/about': About,
+  '/contact': Contact,
+  '/coding': Coding,
+};
 
 
 import MainPage from './components/MainPage';
 
 export default {
   name: 'App',
+
+  data: {
+    currentRoute: window.location.pathname
+  },
+
+  computed: {
+    ViewComponent(){
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+
+  render (h) { return h(this.ViewComponent)},
+
   components: {
     MainPage,
   },
---
+++ teamlearn/src/components/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Contact</h1>
+</template>
+
+
+<script>
+export default {
+    name: 'contact',
+    components: {},
+}
+</script>
+
+
+<style scoped>
+
+</style>
