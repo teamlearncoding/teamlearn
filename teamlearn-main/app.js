f4d36bdaaef9b5615bcf3f7266ae3344d5429670
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-04-15 12:53:55 +0300

    express serves all files now

--- teamlearn-main/app.js
+++ teamlearn-main/app.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', function(req, res) {
-    res.send('Hello world');
-});
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.listen(port, () => {
