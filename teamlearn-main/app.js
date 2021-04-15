48fdef719e8f4542f28f1cd75cca9587c34582e4
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-04-15 14:49:34 +0300

    cool easter egg

--- teamlearn-main/app.js
+++ teamlearn-main/app.js
@@ -3,6 +3,18 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
 
+const members = [
+    {
+        id: 1,
+        name: "Arin Grigoras",
+        email: "aringrigoras@gmail.com", //somekind of easter egg I guess :)
+        status: 'active'
+    }
+];
+
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
